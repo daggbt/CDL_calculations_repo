75aec2f8ace4792238dbdebb2211716f7955ccb4
daggbt <dagem9@gmail.com>
2022-04-30 13:41:20 +0800

    cdl files uploaded

---
+++ CDLconfig.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Ion:
+  name=""
+  charge=""
+  gaussianRadiusAng = 0
+  radiusAng=0
+  polarizability = 0
+  
+
+class Solvent:
+  name = ""
+  radiusAng = 0
+  dielectric = 0
+  reducedDielectric = 0
+
